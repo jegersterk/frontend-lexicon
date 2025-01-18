532b4484451cc6b3f5096fda939ad0f944a76ebf
jegersterk <jegersterk1@gmail.com>
2025-01-18 14:01:38 +0100

    functional bucket list

--- projekt-2025-v03-js-bucket-list/index.js
+++ projekt-2025-v03-js-bucket-list/index.js
@@ -2,32 +2,12 @@
 const bucketLists = document.querySelector("#bucketLists");
 const button = document.querySelector("button");
 const form = document.getElementById("bucketForm");
-// button.addEventListener("click", () => {
-// 	// event.preventDefault();
-// 	const select = document.getElementById("activityCategory");
-// 	const input = document.getElementById("activityName");
 
-// 	if (!document.getElementById(select.value)) {
-// 		const bucketListDiv = document.createElement("div");
-// 		bucketLists.appendChild(bucketListDiv);
-// 		bucketListDiv.textContent = select.value;
-// 		bucketListDiv.appendChild(document.createElement("ul"));
-// 		bucketListDiv.id = select.value;
-// 	}
-// 	const bucketList = document.querySelector(`#${select.value} ul`);
-// 	const bucketListItem = document.createElement("li");
-// 	bucketListItem.textContent = input.value;
-// 	// console.log(!document.querySelector(`#${select.value}`));
-// 	bucketList.appendChild(bucketListItem);
-
-// 	console.log(`bucketList children: ${bucketList.childElementCount}`);
-// 	console.log(select.value);
-// 	console.log(input.value);
-// })
 form.onsubmit = (event) => {
 	// event.preventDefault();
 	const select = document.getElementById("activityCategory");
 	const input = document.getElementById("activityName");
+
 	if (!document.getElementById(select.value)) {
 		const bucketListContainer = document.createElement("div");
 		const bucketList = document.createElement("ul");
@@ -37,13 +17,34 @@ form.onsubmit = (event) => {
 		bucketLists.appendChild(bucketListContainer);
 		bucketListContainer.appendChild(bucketList);
 	}
+
 	const bucketListContainer = document.getElementById(select.value)
 	const bucketList = document.querySelector(`#${select.value} ul`);
 	const bucketListItem = document.createElement("li");
+	const v = document.createElement("span");
+	const x = document.createElement("span");
+
+	v.className = "checkbox checkbox--v";
+	x.className = "checkbox checkbox--x";
+
+	v.onclick = () => {
+		v.classList.toggle('clicked');
+	}
+
+	x.onclick = () => {
+		bucketList.removeChild(bucketListItem);
+		if (bucketList.childElementCount <= 0) {
+			bucketLists.removeChild(bucketListContainer);
+		}
+	}
+
 	bucketListItem.textContent = input.value;
 	bucketList.appendChild(bucketListItem);
 	bucketListContainer.appendChild(bucketList);
-	console.log(input.value);
+	bucketListItem.appendChild(v);
+	bucketListItem.appendChild(x);
+
 	form.reset();
-	return false;
+	console.log(input.value);
+	return false; // samma sak som event.preventDefault();
 }
