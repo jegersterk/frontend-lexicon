f43eaedab79d21ddd300b5a29534a44659213869
jegersterk <jegersterk1@gmail.com>
2025-02-20 18:48:50 +0100

    typescript bootcamp completed

--- 2025-vecka-06/typescript-bootcamp/dist/script.js
+++ 2025-vecka-06/typescript-bootcamp/dist/script.js
@@ -75,4 +75,98 @@ function isLongString(str) {
     return str.length >= 10 ? true : false;
 }
 console.log("uppg 11: ", isLongString(str11));
+// 12
+const arr12 = [1, 3, 2, 6, 5];
+function sortNumbers(arr) {
+    return arr.sort((a, b) => a - b);
+}
+console.log("uppg 12: ", sortNumbers(arr12));
+// 13
+const latitude = 1;
+const longitude = 2;
+const coordinate = [latitude, longitude];
+console.log("uppg 13: ", coordinate);
+// 14
+function isString(arg) {
+    return typeof (arg) === "string";
+}
+console.log("uppg 14: ", isString(1));
+// 15
+function joinStrings(strArr) {
+    let sumOfStr = '';
+    for (str of strArr) {
+        sumOfStr += str;
+    }
+    return sumOfStr;
+}
+const strArr = ["ha ", "he ", "ho"];
+console.log("uppg 15: ", joinStrings(strArr));
+// 16
+function greet(name) {
+    return name.length === 0 ? "hello guest" : "hello " + name;
+}
+console.log("uppg 16: ", greet(""));
+// 17
+function createMultiplier(mult) {
+    return (arg) => arg * mult;
+}
+const multiplier = createMultiplier(5);
+console.log("uppg 17: ", multiplier(3));
+// 18
+function printValue(value) {
+    if (value == null || value == undefined) {
+        console.log("uppg 18: ", "no value");
+    }
+    else {
+        console.log("uppg 18: ", typeof (value));
+    }
+}
+const var18 = null;
+printValue(var18);
+const abook = {
+    title: "a book",
+    author: "dunno",
+    year: 1978
+};
+// 20
+const grade = 150;
+function gradeEvaluator(grade) {
+    return grade >= 50 ? "pass" : "fail";
+}
+console.log("uppg 20: ", gradeEvaluator(grade));
+// 21
+function countdown(count) {
+    const interval = setInterval(() => {
+        console.log("uppg 21: ", count--);
+        count < 0 ? clearInterval(interval) : null;
+    }, 1000);
+}
+// countdown(10);
+// 22
+const word = "blue";
+function createSentence(word) {
+    return `a ${word} car`;
+}
+console.log("uppg 22: ", createSentence(word));
+// 23
+function trafficLight() {
+    const lights = ["red", "yellow", "green"];
+    let i = 0;
+    const interval = setInterval(() => {
+        console.log("uppg 23: ", lights[i % 3]);
+        if (i > 10) {
+            clearInterval(interval);
+        }
+        i++;
+    }, 1000);
+}
+// trafficLight();
+// 24
+function oddEvenArrCheck(arr) {
+    for (const num of arr) {
+        console.log("uppg 24: ", num, isEven(num) ? "even" : "odd");
+    }
+}
+const arr24 = [1, 2, 3, 4, 2, 3, 4, 5];
+oddEvenArrCheck(arr24);
 //# sourceMappingURL=script.js.map
--- 2025-vecka-06/typescript-bootcamp/src/script.ts
+++ 2025-vecka-06/typescript-bootcamp/src/script.ts
@@ -2,7 +2,7 @@
 let nr: number = 2;
 let str: string = "two";
 let bool: boolean = false;
-console.log("uppg 2d: ",nr,str,bool);
+console.log("uppg 2d: ", nr, str, bool);
 
 // 2
 // a Vad betyder (type) inference? 				- att man bestämmer vilken typ värdet är
@@ -12,30 +12,30 @@ console.log("uppg 2d: ",nr,str,bool);
 // e Vad är generics?							- beskriver värdetyp
 
 // 3
-function sum (x:number, y:number) :number{
-	return x+y;
+function sum(x: number, y: number): number {
+	return x + y;
 }
-console.log("uppg 3: ",sum(3,4));
+console.log("uppg 3: ", sum(3, 4));
 
 // 4
-function isEven(x:number) :boolean{
-	if(x%2){
+function isEven(x: number): boolean {
+	if (x % 2) {
 		return false;
 	}
-	else{
+	else {
 		return true;
 	}
 }
-console.log("uppg 4: ",isEven(3));
+console.log("uppg 4: ", isEven(3));
 
 // 5
-const numbers : number[] = [1,2,3,4,4,5,6,6,7];
+const numbers: number[] = [1, 2, 3, 4, 4, 5, 6, 6, 7];
 
 // 6
-function filterEvenNumbers(arr:number[]) :number[]{
+function filterEvenNumbers(arr: number[]): number[] {
 	let newArray: number[] = [];
-	for(let x of arr){
-		if(isEven(x)){
+	for (let x of arr) {
+		if (isEven(x)) {
 			newArray.push(x);
 		}
 	}
@@ -44,10 +44,10 @@ function filterEvenNumbers(arr:number[]) :number[]{
 console.log("uppg 6: ", filterEvenNumbers(numbers));
 
 // 7
-function describeValue(value:string | number){
-	return typeof(value);
+function describeValue(value: string | number) {
+	return typeof (value);
 }
-console.log("uppg 7: ", describeValue(1),describeValue("1"));
+console.log("uppg 7: ", describeValue(1), describeValue("1"));
 
 // 8
 type Person = {
@@ -62,29 +62,139 @@ const snubbe: Person = {
 console.log("uppg 8: ", snubbe);
 
 // 9
-function printPersonInfo(person:Person){
-	console.log("uppg 9: ", "name: ",person.name,", age: ",person.age);
+function printPersonInfo(person: Person) {
+	console.log("uppg 9: ", "name: ", person.name, ", age: ", person.age);
 }
 printPersonInfo(snubbe);
 
 // 10
 enum weekday {
-monday,
-tuesday,
-wednesday,
-thursday,
-friday,
-saturday = "weekend",
-sunday = "weekend"
+	monday,
+	tuesday,
+	wednesday,
+	thursday,
+	friday,
+	saturday = "weekend",
+	sunday = "weekend"
 };
-function isWeekend(day: weekday) :boolean{
+function isWeekend(day: weekday): boolean {
 	return day === "weekend";
 }
 console.log("uppg 10: ", isWeekend(weekday.sunday));
 
 // 11
 const str11: string = "heheaaaaaaa";
-function isLongString(str: string) :boolean{
-	return str.length >= 10? true : false;
+function isLongString(str: string): boolean {
+	return str.length >= 10 ? true : false;
 }
 console.log("uppg 11: ", isLongString(str11))
+
+// 12
+const arr12: number[] = [1, 3, 2, 6, 5];
+function sortNumbers(arr: number[]): number[] {
+	return arr.sort((a, b) => a - b);
+}
+console.log("uppg 12: ", sortNumbers(arr12));
+
+// 13
+const latitude = 1;
+const longitude = 2;
+const coordinate: [number, number] = [latitude, longitude];
+console.log("uppg 13: ", coordinate);
+
+// 14
+function isString(arg: any): boolean {
+	return typeof (arg) === "string";
+}
+console.log("uppg 14: ", isString(1));
+
+// 15
+function joinStrings(strArr: string[]) {
+	let sumOfStr = '';
+	for (str of strArr) {
+		sumOfStr += str;
+	}
+	return sumOfStr;
+}
+const strArr: string[] = ["ha ", "he ", "ho"];
+console.log("uppg 15: ", joinStrings(strArr));
+
+// 16
+function greet(name: string): string {
+	return name.length === 0 ? "hello guest" : "hello " + name;
+}
+console.log("uppg 16: ", greet(""));
+
+// 17
+function createMultiplier(mult: number): (argument: number) => number {
+	return (arg: number) => arg * mult;
+}
+const multiplier: any = createMultiplier(5);
+console.log("uppg 17: ", multiplier(3));
+
+// 18
+function printValue(value: unknown): void {
+	if(value == null || value == undefined){
+		console.log("uppg 18: ", "no value");
+	}else{
+		console.log("uppg 18: ", typeof(value));
+	}
+}
+const var18: any = null;
+printValue(var18);
+
+// 19
+interface book{
+	title: string,
+	author: string,
+	year: number
+}
+const abook: book = {
+	title: "a book",
+	author: "dunno",
+	year: 1978
+}
+
+// 20
+const grade: number = 150;
+function gradeEvaluator(grade:number) : string{
+	return grade >= 50 ? "pass" : "fail";
+}
+console.log("uppg 20: ", gradeEvaluator(grade));
+
+// 21
+function countdown(count:number){
+	const interval: any = setInterval(() => {
+		console.log("uppg 21: ", count--);
+		count < 0 ? clearInterval(interval):null;
+	}, 1000);
+}
+// countdown(10);
+
+// 22
+const word:string = "blue";
+function createSentence(word:string):string{
+	return `a ${word} car`;
+}
+console.log("uppg 22: ", createSentence(word));
+
+// 23
+function trafficLight(){
+	const lights : string[] = ["red","yellow","green"];
+	let i = 0;
+	const interval = setInterval(() => {
+		console.log("uppg 23: ", lights[i%3]);
+		if(i > 10){clearInterval(interval)}
+		i++;
+	},1000);
+}
+// trafficLight();
+
+// 24
+function oddEvenArrCheck(arr:number[]){
+	for(const num of arr){
+		console.log("uppg 24: ",num,isEven(num)?"even":"odd");
+	}
+}
+const arr24:number[] = [1,2,3,4,2,3,4,5];
+oddEvenArrCheck(arr24);
